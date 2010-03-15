8868a49e51db85594960476e78d4fc6d080eda9b
Julio Protzek <julioprotzek@gmail.com>
2010-03-15 20:37:16 -0300

    enviar_email.php resposta amigavel para usuaria

--- enviar_email.php
+++ enviar_email.php
@@ -1,9 +1,34 @@
-<?php
 
-	$email= 'julioprotzek@gmail.com';
-	$assunto= $_POST['assunto'];
-	$mensagem= $_POST['mensagem'];
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>confirmação</title>
+	<!--[if IE]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<style>
+  article, aside, figure, footer, header, hgroup, 
+  menu, nav, section { display: block; }
+</style>
+</head>
+<body>
+	<div class="container">
+		<h1>Conato</h1>
+		
+		<?php
+	error_reporting(0); // desliga todos os erros	
+	$email='alex@unacon.org.br,aloisio@elianapedrosa.com.br';
+	$assunto=$_POST['assunto'];
+	$menssagem=$_POST['mensagem'];
 	
-	mail( $email, $assunto, $mensagem );
-
+	if ( mail( $email, $assunto, $menssagem )):
+	 
 ?>
+			<p>seu email foi enviado com sucesso</p>
+		<?php else:  ?>
+		<p>não foi possível enviar seu email, tente mais tarde</p>
+		<?php endif;  ?>
+	</div>
+</body>
+</html>
